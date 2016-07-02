9a73a523aa935718eb168a53c17d2c012602c276
AthiraJacob <athi.jacob@gmail.com>
2016-07-02 17:16:02 +0530

    Sort images into classes

--- sortImages.py
+++ sortImages.py
@@ -8,14 +8,27 @@ nP = 47 # No. of patients
 nI = 120 #No. of images per patient
 nClasses = 2
 fold = 'G:/athi/acads/kaggleUS/train' #data folder
+destFold = ''
+os.mkdir(destfold+'/0')
+os.mkdir(destfold+'/1')
 
 for p in range(1,nP):
 	for i in range(1,nI):
 		# read and store image
 		imgName = fold+'/'+str(p)+'_'+str(i)+'.tif'
+		
 		# read and store mask variable
 		maskName = fold+'/'+str(p)+'_'+str(i)+'_'+'mask.tif'
-		mask = Image.open(maskName)
+		maskImg = Image.open(maskName)
 		mask = np.array(mask)
 		if np.sum(mask)>0:
+			className = '1'
+		else:
+			className = '0'
+
+		newImageName = destfold+'/'+className+'/'+str(p)+'_'+str(i)+'.tif'
+		newMaskName = destfold+'/'+className'/'+str(p)+'_'+str(i)+'_'+'mask.tif'
+		Image.open(imgName).save(newImageName)
+		maskImg.save(newMaskName)
+
 			
